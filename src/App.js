6ec8c93ef406a9549cc9b6321b3f1888e0422a40
Student <jsitbon@qcc.cuny.edu>
2019-07-16 17:24:53 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoList extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos:[],
+      current: "",
+    };
+  }
+  
+  handleChange = event => {
+    this.setState({
+      current:event.target.value
+    })
+    console.log(event.target.value)
+  };
+
+  addItem = event => {
+    event.preventDefault();
+    this.setState({
+      todos: [...this.state.todos,this.state.current],
+      current: ''
+    })
+    console.log("Successfully added",this.state.current,"to the list!")
+  }
+
+  imDone = index =>{
+    var temp = [...this.state.todos]
+    console.log("line 31:",index)
+      temp.splice(index,1)
+      this.setState({todos:temp})
+  }
+
+  render(){
+    const theList = this.state.todos.map((todos, index) => <span key={index} className="listItem"><li>{todos}</li><button onClick={()=>{this.imDone(index)}}>DELETE</button></span>);
+    return(
+      <div className = 'container'>
+      <form>
+      <label htmlFor="taskName">Task Name:</label>
+      <input onChange = {this.handleChange} name="taskName" type="text" placeholder="Add a task here" value={this.state.current}></input>
+      <button type="submit" onClick={this.addItem}>Add to list</button>
+      </form>
+      <ul>{theList}</ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TodoList;
