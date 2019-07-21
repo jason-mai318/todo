68e969b4c5b39a0f9a7bbf787640f8aa872e3872
Jason <jasonmai318@gmail.com>
2019-07-21 13:12:13 -0400

    added name option

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import "./App.css";
 
 class TodoList extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      todos:[],
+      todos: [],
       current: "",
+      temp: "",
+      user: "Your"
     };
   }
-  
+
+  handleName = event => {
+    this.setState({
+      temp: event.target.value
+    })
+    console.log(event.target.value)
+  };
+
+  setName = event => {
+    event.preventDefault();
+    if (this.state.temp !== "") {
+      this.setState({
+        user: this.state.temp + '\'s',
+        temp: ""
+      })
+    }
+    else {
+      this.setState({
+        user: 'Your',
+        temp: ""
+      })
+    }
+  };
+
   handleChange = event => {
     this.setState({
-      current:event.target.value
+      current: event.target.value
     })
     console.log(event.target.value)
   };
@@ -19,29 +45,50 @@ class TodoList extends Component {
   addItem = event => {
     event.preventDefault();
     this.setState({
-      todos: [...this.state.todos,this.state.current],
+      todos: [...this.state.todos, this.state.current],
       current: ''
     })
-    console.log("Successfully added",this.state.current,"to the list!")
+    console.log("Successfully added", this.state.current, "to the list!")
   }
 
-  imDone = index =>{
+  imDone = (index) => {
     var temp = [...this.state.todos]
-    console.log("line 31:",index)
-      temp.splice(index,1)
-      this.setState({todos:temp})
+    temp.splice(index, 1)
+    this.setState({ todos: temp })
   }
 
-  render(){
-    const theList = this.state.todos.map((todos, index) => <span key={index} className="listItem"><li>{todos}</li><button onClick={()=>{this.imDone(index)}}>DELETE</button></span>);
-    return(
-      <div className = 'container'>
-      <form>
-      <label htmlFor="taskName">Task Name:</label>
-      <input onChange = {this.handleChange} name="taskName" type="text" placeholder="Add a task here" value={this.state.current}></input>
-      <button type="submit" onClick={this.addItem}>Add to list</button>
-      </form>
-      <ul>{theList}</ul>
+  editRequest = (index) => {
+    var temp = [...this.state.todos]
+    temp = this.state.todos.map((todos, idx) => {
+      if (idx === index) {
+        return "if-ed"
+      }
+      else {
+        return temp
+      }
+    });
+    this.setState({ todos: temp })
+  }
+
+  render() {
+    const theList = this.state.todos.map((todos, index) => <span key={index} className="listItem"><ul><button onClick={() => { this.imDone(index) }}>DELETE</button>{todos}<button onClick={() => { this.editRequest(index) }}>EDIT</button></ul></span>);
+    return (
+      <div className='container'>
+        <div className="navBar">
+          <div id="nameText"><b>Set Your Name Here:</b></div>
+          <input id="nameInputBox" onChange={this.handleName} name="userName" type="text" placeholder="Enter your name" value={this.state.temp}></input>
+          <button type="submit" onClick={this.setName}>Enter</button>
+        </div>
+        <div className="thelist">
+          <h1><u>{this.state.user} To-Do List</u></h1>
+          <p><b>{this.state.todos.length} things left to do.</b></p>
+          <form>
+            <label htmlFor="taskName">Task Name:</label>
+            <input onChange={this.handleChange} name="taskName" type="text" placeholder="Add a task here" value={this.state.current}></input>
+            <button type="submit" onClick={this.addItem}>Add to list</button>
+          </form>
+          <ul>{theList}</ul>
+        </div>
       </div>
     );
   }
