e06b28170dcdc737698fd98add16a6f23edeaf53
Student <jsitbon@qcc.cuny.edu>
2019-07-29 11:13:43 -0400

    Updates

---
+++ precache-manifest.7a65e964e437898823f37c00190592e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c58fd6bff26d0231a10a5a22ac5844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d95f5be822781a3e227",
+    "url": "/static/css/main.3a93972a.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "4d95f5be822781a3e227",
+    "url": "/static/js/main.a9c5d38e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
